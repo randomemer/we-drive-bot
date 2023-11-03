e4d513946ac8febaa57e84bd037b6da01b5da26f
Shashank Pathipati <pshashank569@gmail.com>
2023-11-03 22:53:45 +0530

    minor

--- knex/migrations/20231103101531_guilds.js
+++ knex/migrations/20231103101531_guilds.js
@@ -4,10 +4,10 @@
  */
 exports.up = async function (knex) {
   // 1. Fetch all records
-  // const guilds = await knex.select("*").from("servers");
-  const guilds = [];
+  const guilds = await knex.select("*").from("servers");
 
   // 2. Perform DDL queries
+  await knex.schema.dropTableIfExists("guilds");
   await knex.schema.dropTableIfExists("servers");
 
   await knex.schema.createTable("servers", (table) => {
@@ -44,17 +44,23 @@ exports.up = async function (knex) {
   });
 
   // 3. Perform DML queries
-  return knex.transaction(async (trx) => {
+  await knex.transaction(async (trx) => {
     const servers = new Map();
 
     for (const guild of guilds) {
       if (guild.mc_server) {
         servers.set(guild.mc_server, {
-          id: guild.mc_servers,
+          id: guild.mc_server,
           backup_cron: guild.backup_cron,
         });
       }
+    }
 
+    for (const server of servers.values()) {
+      await trx.insert(server).into("servers");
+    }
+
+    for (const guild of guilds) {
       await trx
         .insert({
           id: guild.id,
@@ -66,10 +72,6 @@ exports.up = async function (knex) {
         })
         .into("guilds");
     }
-
-    for (const server of servers.values()) {
-      await trx.insert(server).into("servers");
-    }
   });
 };
 
@@ -78,6 +80,35 @@ exports.up = async function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = async function (knex) {
+  const guilds = await knex.select("*").from("guilds");
+  const servers = await knex.select("*").from("servers");
+
   await knex.schema.dropTable("guilds");
   await knex.schema.dropTable("servers");
+
+  await knex.schema.createTable("servers", (table) => {
+    table.string("id").primary();
+    table.string("mc_role");
+    table.string("mc_channel");
+    table.string("mc_server");
+    table.string("backup_cron").defaultTo("0 0 * * *");
+
+    table
+      .datetime("created_at")
+      .notNullable()
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP()"));
+    table
+      .datetime("updated_at")
+      .notNullable()
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP()"));
+  });
+
+  for (const guild of guilds) {
+    const server = servers.find((srv) => srv.id === guild.mc_server);
+
+    guild.mc_server = server.id;
+    guild.backup_cron = server.backup_cron;
+
+    await knex.table("servers").insert(guild);
+  }
 };
